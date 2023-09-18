412a90c402edc00f691f4f8ad482cb1835c9639b
Faeze <faezeahmadi7173@gmail.com>
2023-09-18 13:49:47 +0330

    45min

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,21 @@
 import './style.css';
-
 const app = document.querySelector<HTMLDivElement>("#app");
 
-// const h1 = document.createElement("h1");
-// h1.classList.add("bg-pink-600");
-
-// const span = document.createElement("span");
-// span.innerHTML = "hello vite";
-
-// h1.appendChild(span);
-
-// app!.appendChild(h1);
-console.log(app);
 
 type BoxProps = {
   element: string;
   att?: Element;
-  children?: Element;
+  children?: string | string[] | Element | Element[];
 };
 
 const Box = (props: BoxProps) => {
   const el = document.createElement(props.element);
   const span = document.createElement("span");
   span.innerHTML = "hello vite";
-  el.append(span, "gf", "hg")
+  el.append(span, "gf", " ", "hg")
+  el.append(props.children)
   return el;
 };
 
-console.log(Box({ element: "h1" }));
-
+app!.append(Box({ element: "h1", children: "hello children" }));
+app!.append(Box({ element: "mark", children: "zahra" }));
