510552209fac845dd8989f9b1dcc8437920ffa5c
Faeze <faezeahmadi7173@gmail.com>
2023-09-18 13:56:37 +0330

    50min

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,12 @@ const Box = (props: BoxProps) => {
   const el = document.createElement(props.element);
   const span = document.createElement("span");
   span.innerHTML = "hello vite";
-  el.append(span, "gf", " ", "hg")
-  el.append(props.children)
+  el.append(span, " ", "gf", " ", "hg")
+
+  if (!Array.isArray(props.children)) el.append(props.children!);
+  else el.append(...props.children)
   return el;
 };
 
 app!.append(Box({ element: "h1", children: "hello children" }));
-app!.append(Box({ element: "mark", children: "zahra" }));
+app!.append(Box({ element: "mark", children: ["zahra", " ", "faeze", " ", "narges"] }));
