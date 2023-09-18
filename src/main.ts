78eeb01e3376e76efcbf6662083af32d11071edc
Faeze <faezeahmadi7173@gmail.com>
2023-09-18 13:24:55 +0330

    feat/faeze/learning-vite/const

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import './style.css';
 
 const app = document.querySelector<HTMLDivElement>("#app");
+
+const h1 = document.createElement("h1");
+h1.classList.add("bg-pink-600");
+
+const span = document.createElement("span");
+span.innerHTML = "hello vite";
+
+h1.appendChild(span);
+
+app!.appendChild(h1);
+
+console.log(app);
