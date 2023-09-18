6683deb5feb60218ae1c20b1675ce6478b7d1e62
Faeze <faezeahmadi7173@gmail.com>
2023-09-18 14:11:42 +0330

    1:2min

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,28 @@ const app = document.querySelector<HTMLDivElement>("#app");
 
 type BoxProps = {
   element: string;
-  att?: Element;
+  attr?: any;
   children?: string | string[] | Element | Element[];
 };
 
 const Box = (props: BoxProps) => {
+  console.log(props.attr);
   const el = document.createElement(props.element);
-  const span = document.createElement("span");
-  span.innerHTML = "hello vite";
-  el.append(span, " ", "gf", " ", "hg")
-
+  el.className = props.attr?.className;
   if (!Array.isArray(props.children)) el.append(props.children!);
-  else el.append(...props.children)
+  else el.append(...props.children);
   return el;
 };
 
-app!.append(Box({ element: "h1", children: "hello children" }));
-app!.append(Box({ element: "mark", children: ["zahra", " ", "faeze", " ", "narges"] }));
+// app!.append(Box({ element: "h1", children: "hello children" }));
+// app!.append(Box({ element: "mark", children: ["zahra", " ", "faeze", " ", "narges"] }));
+
+app!.append(
+  Box({
+    element: "ul", 
+    attr: {
+      className: "bg-pink-500"
+    },
+    children: Box({ element: "li", children: "link" })
+  })
+);
